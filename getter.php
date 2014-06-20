d3c91901459faf729df57069503203aded9368ec
Pablo Botta <pabloluisbotta@gmai.com>
2014-06-19 21:38:40 -0300

    Reduje el HTML que empuja el getter, entonces cambio el DOM

--- getter.php
+++ getter.php
@@ -1,31 +1,45 @@
 <?php
-    //extract data from the post
-    extract($_POST);
+function DOMinnerHTML(DOMNode $element) 
+{ 
+    $innerHTML = ""; 
+    $children  = $element->childNodes;
 
-    //set POST variables
-    $url = 'http://mundialconamigos.com/index.php?id=18';
-    $fields = array(
-        'cual' => urlencode($_GET["grupo"]),
-    );
-
-    //url-ify the data for the POST
-    foreach($fields as $key=>$value) { 
-        $fields_string .= $key.'='.$value.'&'; 
+    foreach ($children as $child) 
+    { 
+        $innerHTML .= $element->ownerDocument->saveHTML($child);
     }
-    rtrim($fields_string, '&');
 
-    //open connection
-    $ch = curl_init();
+    return $innerHTML; 
+} 
+//extract data from the post
+extract($_POST);
 
-    //set the url, number of POST vars, POST data
-    curl_setopt($ch,CURLOPT_URL, $url);
-    curl_setopt($ch,CURLOPT_POST, count($fields));
-    curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+//set POST variables
+$url = 'http://mundialconamigos.com/index.php?id=18';
+$fields = array(
+    'cual' => urlencode($_GET["grupo"]),
+);
 
-    //execute post
-    $result = curl_exec($ch);
+//url-ify the data for the POST
+foreach($fields as $key=>$value) { 
+    $fields_string .= $key.'='.$value.'&'; 
+}
+rtrim($fields_string, '&');
 
-    //close connection
-    curl_close($ch);
+//open connection
+$ch = curl_init();
 
-    print $result;
+//set the url, number of POST vars, POST data
+curl_setopt($ch,CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_POST, count($fields));
+curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//execute post
+$result = curl_exec($ch);
+
+//close connection
+curl_close($ch);
+$DOM = new DOMDocument;
+$DOM->loadHTML($result);
+$pos = $DOM->getElementById('posiciones_grupo');
+print DOMinnerHTML($pos);
